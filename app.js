979af81d401d285d169d397888c006175ead23ec
looneysky <63257090+looneysky@users.noreply.github.com>
2024-11-10 02:08:10 +0200

    update v6

--- app.js
+++ app.js
@@ -1,23 +1,44 @@
 const express = require('express');
 const path = require('path');
+require('dotenv').config(); // Загружаем переменные окружения из .env
 
 const app = express();
 const port = 3000;
 
-// Указываем папку для обслуживания статических файлов
+// Указываем директорию для статических файлов и для EJS шаблонов
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'public', 'views'));  // Указываем EJS искать шаблоны в public/views
 
-// Обрабатываем запрос на корневой маршрут и отправляем index.html
+// Главный маршрут с рендерингом EJS
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.render('index', {
+    title: process.env.SITE_TITLE,
+    description: process.env.SITE_DESCRIPTION,
+    keywords: process.env.SITE_KEYWORDS,
+    url: process.env.SITE_URL,
+    image: process.env.SITE_IMAGE,
+    twitter: process.env.TWITTER_URL,
+    telegram: process.env.TELEGRAM_URL,
+    channel: process.env.CHANNEL_URL
+  });
 });
 
-// Обрабатываем запрос на корневой маршрут и отправляем index.html
+// Главный маршрут с рендерингом EJS
 app.get('/start', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'start.html'));
+  res.render('start', {
+    title: process.env.SITE_TITLE,
+    description: process.env.SITE_DESCRIPTION,
+    keywords: process.env.SITE_KEYWORDS,
+    url: process.env.SITE_URL,
+    image: process.env.SITE_IMAGE,
+    twitter: process.env.TWITTER_URL,
+    telegram: process.env.TELEGRAM_URL,
+    channel: process.env.CHANNEL_URL
+  });
 });
 
-// Запускаем сервер
+// Запуск сервера
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
